a3a0e71d6c6d3c3827deebcf741e714c04ade95c
jacace <javierandrescaceres@gmail.com>
2021-03-24 09:44:44 +0000

    Removed unused packages

--- src/main/java/com/examples/MySinkConnector.java
+++ src/main/java/com/examples/MySinkConnector.java
@@ -2,10 +2,8 @@ package com.examples;
 
 import java.util.List;
 import java.util.Map;
-
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.Task;
-import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.sink.SinkConnector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/com/examples/MySinkConnectorConfig.java
+++ src/main/java/com/examples/MySinkConnectorConfig.java
@@ -4,9 +4,7 @@ import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.config.ConfigDef.Type;
 import org.apache.kafka.common.config.ConfigDef.Importance;
-
 import java.util.Map;
-
 
 public class MySinkConnectorConfig extends AbstractConfig {
 
